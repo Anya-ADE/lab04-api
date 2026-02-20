73e493a803cb0a1b5fd20d5d01976c8d0de19355
anya andrews <2019120080@ub.edu.bz>
2026-02-20 17:33:28 -0600

    Part 2: The Custom Response Writer

--- main.go
+++ main.go
@@ -1,15 +1,41 @@
 package main
 
-package main
 import (
-"fmt"
-"log"
-"log/slog"
-"net/http"
-"os"
-"time"
+	"net/http"
 )
 
-func main () {
-	
+/* If a handler calls w.Write([]byte("ok"))
+without calling w.WriteHeader() first, what status code
+does the client receive? What will rw.statusCode hold if
+you pre-set it to http.StatusOK? */
+
+/*Answer: If a handler calls w.Write() without an explicit call to w.WriteHeader(),
+the client receives a status code of 200 OK. If the pre-set rw.statusCode is set to
+http.StatusOK, it will hold the value 200.*/
+
+type responseWriter struct {
+	http.ResponseWriter     // embed the real writer
+	statusCode          int // captured status code
+}
+
+// WriteHeader intercepts the status code before forwarding it.
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		rw := &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+		rw.Write([]byte("ok"))
+		println("Captured Status Code:", rw.statusCode)
+	})
+
+	err := http.ListenAndServe(":4000", mux)
+	if err != nil {
+		panic(err)
+	}
+
 }
